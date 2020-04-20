82efc2b5e0596b18db419779ca25e470f7977fda
9adam <lengyel.adam95@gmail.com>
2020-04-20 23:43:47 +0200

    heroku db props

--- db/db.js
+++ db/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'filmfanatics'
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'bacbdce0cadc1a',
+    password: 'fbd6f069',
+    database: 'heroku_6647cfa7bfae13e'
 });
 
 connection.connect((err) => {
