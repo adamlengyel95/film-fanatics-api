cb284d6e5120f7d282871502fa76bf06e2a6f098
9adam <lengyel.adam95@gmail.com>
2020-05-05 20:04:27 +0200

    Updated db props

--- db/db.js
+++ db/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'v3test2'
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'b135a4706f2da7',
+    password: 'eeb81547',
+    database: 'heroku_8c6087eb51aad31'
 });
 
 module.exports = connection;
