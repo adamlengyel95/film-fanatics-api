58433a51faeaeebe9c248ee16dd8950bf3a9a0a0
9adam <lengyel.adam95@gmail.com>
2020-05-06 21:00:10 +0200

    origin set

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cookieSession({
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://filmfanatics-client.rabit.hu');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
